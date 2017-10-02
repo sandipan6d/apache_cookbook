a0712a2d19a10cc0be36aca73f418de100959f09
Sandipan Das <sandipan@Sandipans-MBP.fios-router.home>
2017-10-02 02:05:47 -0400

    Added notify resource

--- recipes/server.rb
+++ recipes/server.rb
@@ -8,10 +8,12 @@ ip_address = node['ipaddress']
 memory = node['memory']['total']
 hostname = node['hostname']
 
-
-
 package 'httpd' do
   action :install
+end
+
+remote_file '/var/www/html/myimage.png' do
+  source 'http://www.freepngimg.com/download/facebook/1-2-facebook-download-png.png'  
 end
 
 template '/var/www/html/index.html' do
@@ -23,6 +25,7 @@ template '/var/www/html/index.html' do
     :hostname => "#{hostname}"
   )
   action :create
+  notifies :restart, 'service[httpd]', :immediately
 end
 
 service 'httpd' do
